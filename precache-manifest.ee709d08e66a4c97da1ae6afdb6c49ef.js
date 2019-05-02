6415d9435779c07e08cb369d456cd846e3b3e01a
bank8426 <bank_55555@hotmail.com>
2019-05-02 18:22:51 +0700

    Updates

---
+++ precache-manifest.ee709d08e66a4c97da1ae6afdb6c49ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52be9025f45dad57e68c4442e904ec08",
+    "url": "/react-counter-app/index.html"
+  },
+  {
+    "revision": "d0283dcf7a30fad1293d",
+    "url": "/react-counter-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-counter-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d0283dcf7a30fad1293d",
+    "url": "/react-counter-app/static/js/main.bb13e259.chunk.js"
+  },
+  {
+    "revision": "171053235920d89b1d38",
+    "url": "/react-counter-app/static/js/runtime~main.6eff6206.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-counter-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
