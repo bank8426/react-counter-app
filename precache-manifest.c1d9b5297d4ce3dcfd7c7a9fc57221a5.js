3b1502c942d427eff0e89b834f36b8d34c0900ea
bank8426 <bank_55555@hotmail.com>
2019-05-07 13:04:45 +0700

    Updates

---
+++ precache-manifest.c1d9b5297d4ce3dcfd7c7a9fc57221a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cf1e231d63f4709332ecbbaee08b10",
+    "url": "/react-counter-app/index.html"
+  },
+  {
+    "revision": "b2d3ed89bd029342b000",
+    "url": "/react-counter-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "db07734ceaa67f6c6d79",
+    "url": "/react-counter-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "b2d3ed89bd029342b000",
+    "url": "/react-counter-app/static/js/2.eb088328.chunk.js"
+  },
+  {
+    "revision": "db07734ceaa67f6c6d79",
+    "url": "/react-counter-app/static/js/main.62cc11c5.chunk.js"
+  },
+  {
+    "revision": "171053235920d89b1d38",
+    "url": "/react-counter-app/static/js/runtime~main.6eff6206.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-counter-app/precache-manifest.ee709d08e66a4c97da1ae6afdb6c49ef.js"
+  "/react-counter-app/precache-manifest.c1d9b5297d4ce3dcfd7c7a9fc57221a5.js"
 );
 
 self.addEventListener('message', (event) => {
